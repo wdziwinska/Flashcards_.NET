d1a676972b02b56da9fc19f0ed0aac1a5d0eeb0e
G-Blazkowicz <95439533+G-Blazkowicz@users.noreply.github.com>
2022-01-07 19:19:11 +0100

    Zakomentowalem licznik

--- Fiszki_projekt/PhrasesDBImplementation.cs
+++ Fiszki_projekt/PhrasesDBImplementation.cs
@@ -21,16 +21,17 @@ namespace Fiszki_projekt
             using (var reader = new StreamReader(@"BazaSlowek.csv"))
             {
                 reader.ReadLine();
-                int licznik = 0;
+                //int licznik = 0;
                 var retList = new List<(string,string)>();
-                while (!reader.EndOfStream && licznik<10)
+                //while (!reader.EndOfStream && licznik<10)
+                while (!reader.EndOfStream)
                 {
                     var lines = reader.ReadLine();
                     var values = lines.Split(";");
                     //System.Diagnostics.Debug.WriteLine("Slowo: " + values[firstLanguageId] + ", tlumaczenie: " + values[secondLangueId]);
                     (string,string) tuple = (values[firstLanguageId], values[secondLangueId]);
                     retList.Add(tuple);
-                    licznik++; 
+                    //licznik++; 
                 }
                 reader.Close();
                 return retList;
